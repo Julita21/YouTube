cd64fc19ec91998d6dc7133d1f365b4a58c786bf
Julita21 <julitkazmuda@wp.pl>
2022-09-26 09:34:04 +0200

    add docker-compose

--- youtube/settings.py
+++ youtube/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'rest_framework',
     'ideas.apps.IdeasConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -77,8 +78,12 @@ WSGI_APPLICATION = 'youtube.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'youtube',
+        'USER': 'youtube',
+        'PASSWORD':'youtube123',
+        'HOST':'db',
+        'PORT':'5432',
     }
 }
 
